1e70dfb0c1d016974fbe2dcd4171371ba3041f9c
Arne Van Den Kerchove <arne@vandenkerchove.com>
2025-05-08 12:08:46 +0200

    Update README, format

--- README.md
+++ README.md
@@ -1,12 +1,63 @@
 # brainlink-lsl
 
-A python script to connect the MacroTelect Brainlink Pro to LabStreamingLayer
+
+A python script to connect the Macrotelect
+[Brainlink Pro](https://o.macrotellect.com/BrainLinkPro.html) EEG headband to
+[LabStreamingLayer](https://labstreaminglayer.readthedocs.io/)
 for real-time brain-computer interfacing.
 
-## Dependencies
+## Installation and dependencies
 
-`lib` contains the necessary compiled libraries to parse the data read out from
-the BrainLink Pro, but MacroTelect only makes these available for Windows or
-MacOS.
 
-## Setup
+For installation with `conda`, use
+```sh
+conda env create --file environment.yml
+conda activate brainlink-lsl
+```
+This will automatically install the necessary python dependencies and `liblsl`.
+
+The `lib` directory contains the necessary compiled libraries obtained from
+https://github.com/Macrotellect/BrainLinkParser-Python to parse the data read
+out from the BrainLink Pro, but Macrotelect only makes these available for
+Windows and MacOS, Linux is not supported.
+
+## Usage
+
+Connect to the Brainlink Pro as a Bluetooth audio device. A serial port should then
+come available on your device through which the script can read the data.
+
+Next, use the command `python brainlink_lsl.py` with the following options depending
+on your setup:
+```
+Usage: brainlink_lsl.py [OPTIONS]
+
+Options:
+  -d, --device TEXT  The serial port device to use. Defaults to 'COM3'
+  -b, --headband     Pass this option to indicate that the flexible headband
+                     with extra heart rate and temperature sensor is used,
+                     which decreases the effective EEG sampling rate from 512
+                     to 484 Hz if sensors make contact.
+  --help             Show this message and exit.
+```
+
+Some downstream applications, like time-locked BCI paradigms implemented in the
+OpenVibe software, need the nominal sampling rate advertised by
+the LSL stream to closely match the effective sampling rate of the stream.
+While Macrotelect advertises a 512 Hz sampling rate for the BrainLink Pro,
+closer inspection reveals that the average effective sampling rate for our device was closer to 513.78 Hz
+when using the rigid head brace. When using the flexible headband hardware, the
+mismatch is even larger and the average effective sampling rate for our device
+was 484.30 Hz.
+
+Using the script `determine_srate.py`, you can examine the average
+effective sampling rate for your device and adjust the values in the `brainlink_lsl.py` script if necessary.
+First, run `python brainlink_lsl.py`. In another terminal, run `python determine_srate.py`
+and select the stream for which you want to determine the sampling rate.
+
+## Output streams
+
+Upon running `python brainlink_lsl`, the following streams are made available through LSL:
+* `BrainLink-<COM port>-eeg`: the raw EEG data at 484.30 Hz for the headband setup, or 513.78 Hz for the head brace setup.
+* `BrainLink-<COM port>-gyro`: the gyroscope's x, y, and z axis output at 2.8 Hz.
+* `BrainLink-<COM port>-physio`: physiological data like heart rate and
+   temperature at 1 Hz.
--- brainlink_lsl.py
+++ brainlink_lsl.py
@@ -1,96 +1,111 @@
-from lib.BrainLinkParser import BrainLinkParser
-from serial import Serial
-from pylsl import StreamInfo, StreamOutlet
-import time
 import logging
 import sys
+import time
+
 import click
+from pylsl import StreamInfo, StreamOutlet
+from serial import Serial
+
+from lib.BrainLinkParser import BrainLinkParser
 
 logging.basicConfig(
     stream=sys.stdout,
     level=logging.DEBUG,
-    format='%(asctime)s %(levelname)-8s %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S'
+    format="%(asctime)s %(levelname)-8s %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
 )
 
 
 class EEGHandler:
-    
+
     def __init__(self, dev, headband):
-        srate = 484.3 if headband else 513.78
+        srate = 484.30 if headband else 513.78
         info = StreamInfo(
-            name='BrainLink-eeg',
-            type='EEG',
+            name="BrainLink-eeg",
+            type="EEG",
             channel_count=1,
             nominal_srate=srate,
-            channel_format='float32',
-            source_id=f'BrainLink-{dev}-eeg'
+            channel_format="float32",
+            source_id=f"BrainLink-{dev}-eeg",
         )
         self._outlet = StreamOutlet(info)
-    
+
     def __call__(self, eeg):
         self._outlet.push_sample([float(eeg)])
 
+
 class GyroHandler:
-        
+
     def __init__(self, dev):
         info = StreamInfo(
-            name='BrainLink-gyro',
-            type='Gyro',
+            name="BrainLink-gyro",
+            type="Gyro",
             channel_count=3,
             nominal_srate=2.8,
-            channel_format='float32',
-            source_id=f'BrainLink-{dev}-gyro'
-        )    
+            channel_format="float32",
+            source_id=f"BrainLink-{dev}-gyro",
+        )
         self._outlet = StreamOutlet(info)
-     
+
     def __call__(self, x, y, z):
-        self._outlet.push_sample([x,y,z])
+        self._outlet.push_sample([x, y, z])
+
 
 class ExtraInfoHandler:
-    
+
     def __init__(self, dev):
         info = StreamInfo(
-            name='BrainLink-physio',
-            type='Physio',
+            name="BrainLink-physio",
+            type="Physio",
             channel_count=2,
             nominal_srate=1,
-            channel_format='float32',
-            source_id=f'BrainLink-{dev}-physio'
+            channel_format="float32",
+            source_id=f"BrainLink-{dev}-physio",
         )
         self._outlet = StreamOutlet(info)
         self._logger = logging.getLogger()
-    
+
     def __call__(self, data):
-        self._logger.log(logging.INFO, f'version: {data.version}, battery: {data.battery}, ap: {data.ap}')
+        self._logger.log(
+            logging.INFO,
+            f"version: {data.version}, battery: {data.battery}, ap: {data.ap}",
+        )
         self._outlet.push_sample([float(data.heart), float(data.temperature)])
+
 
 @click.command()
 @click.option(
-    '-d', '--device',
-    default='COM3',
-    help="""The serial port device to use. Defaults to 'COM3'"""
+    "-d",
+    "--device",
+    default="COM3",
+    help="""The serial port device to use. Defaults to 'COM3'""",
 )
 @click.option(
-    '--headband', '-b',
+    "--headband",
+    "-b",
     is_flag=True,
     help="""
         Pass this option to indicate that the flexible headband with extra heart rate
         and temperature sensor is used, which decreases the effective EEG sampling
         rate from 512 to 484 Hz if sensors make contact.
-    """
+    """,
 )
 def brainlink_lsl(device, headband):
     ser = Serial(device, baudrate=115200, timeout=0)
-    parser = BrainLinkParser(None, ExtraInfoHandler(device), GyroHandler(device), None, EEGHandler(device, headband))
+    parser = BrainLinkParser(
+        None,
+        ExtraInfoHandler(device),
+        GyroHandler(device),
+        None,
+        EEGHandler(device, headband),
+    )
 
     while True:
         while ser.in_waiting:
             line = ser.readline()
             parser.parse(line)
-        time.sleep((1/512)/3)
-        
-            
+        time.sleep((1 / 512) / 3)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     brainlink_lsl()
